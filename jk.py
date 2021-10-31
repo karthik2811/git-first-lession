03c418746d52a4eea6f37aa73d6ced66e2a8dad0
karthik <karthicomrads@gmail.com>
2021-10-31 07:53:19 +0530

    Today practice

---
+++ jk.py
@@ -0,0 +1,89 @@
+message = 'Welcome to Python'
+print(message)
+
+
+message1 = 'Welcome'
+message2 = 'to Python World'
+print(message1 + message2)  
+
+no1 = 10
+no2 = 20
+print(no1 + no2) 
+
+
+no1, no2 = 10,20
+print(no1)
+print(no2)
+print(id(no1))
+print(id(no2))
+print(type(no1))
+print(type(no2))
+
+no3 = 4.5
+print(type(no3))
+name = 'madhan'
+print(type(name))
+
+
+result = True
+print(result)
+print(type(result))
+result = False
+result = True
+print(result)
+print(type(result))
+
+print(True+True)
+print(True+False)
+print(False+False)
+print(False+True)
+
+name = 'madhan'
+    
+print(name)
+print(type(name))
+print(name[0])  
+print(name[1])  
+print(name[5])  
+
+print(name[-1])
+print(name[-6])
+
+sentence = '''today's temperature is 35 degree celcius'''
+print(sentence)
+
+
+sentence = """today's temperature is 35 degree celcius and 
+I read the book "Alchemist"""
+print(sentence)
+
+sentence = '''today's temperature is 35 degree celcius and 
+I read the book "Alchemist" '''
+print(sentence)
+
+name = 'abcdefghijklmnopqrstuvwxyz'
+print(name[0:5])
+print(name[:5])
+print(name[3:8])
+print(name[:])
+print(name[-5:-1])
+
+
+name = 'abcdefghijklmnopqrstuvwxyz'
+
+print(name[-1:])
+print(name[15:-2])
+print(name[18:-5])
+
+name = 'abcdefghijklmnopqrstuvwxyz'
+
+print(name[::])
+print(name[::2])
+print(name[::3])
+print(name[1::2])
+print(name[1::3])
+print(name[::-1])
+
+print(name[-26:-1])
+print(name[-10:-5])  
+print(name[-26:-1:2])
